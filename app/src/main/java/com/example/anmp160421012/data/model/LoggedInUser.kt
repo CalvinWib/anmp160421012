e29c09684f38a5fdd81f72bf0dab74190108f46a
Calvin <s160421012@student.ubaya.ac.id>
2024-02-19 11:25:17 +0700

    login activity ditambahkan

---
+++ app/src/main/java/com/example/anmp160421012/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package com.example.anmp160421012.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
